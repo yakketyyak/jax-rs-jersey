fd5bd4375ffdf9588137ddbb73456136e287ec12
Patrick BEUGRE <pt.beugre@gmail.com>
2020-04-13 01:04:37 +0200

    ignore null value dto

--- src/main/java/ci/pabeu/rs/helper/dto/UserDto.java
+++ src/main/java/ci/pabeu/rs/helper/dto/UserDto.java
@@ -3,12 +3,15 @@ package ci.pabeu.rs.helper.dto;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 
 @Data
 @EqualsAndHashCode
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
