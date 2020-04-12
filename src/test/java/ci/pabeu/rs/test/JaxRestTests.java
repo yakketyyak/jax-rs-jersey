5f521228d03e00d378d49d386609b426643dd37b
Patrick BEUGRE <pt.beugre@gmail.com>
2020-04-13 01:11:25 +0200

    add test folder

---
+++ src/test/java/ci/pabeu/rs/test/JaxRestTests.java
@@ -0,0 +1,14 @@
+package ci.pabeu.rs.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JaxRestTests {
+
+	@Test
+	public void contextLoads() {
+		assertEquals(true, Boolean.TRUE);
+	}
+
+}
